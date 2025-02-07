6659bd000e128e4c0af37d3d331b73aaa7a742ad
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-02-07 21:17:13 +0100

    Try Add WebView2 for net desktop

--- src/Frontend/Supporter_Uno/Supporter_Uno/Presentation/MainPage.xaml.cs
+++ src/Frontend/Supporter_Uno/Supporter_Uno/Presentation/MainPage.xaml.cs
@@ -1,9 +1,16 @@
-﻿namespace Supporter_Uno.Presentation;
+namespace Supporter_Uno.Presentation;
 
 public sealed partial class MainPage : Page
 {
     public MainPage()
     {
         this.InitializeComponent();
+    }
+
+    protected override async void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+        await MyWebView.EnsureCoreWebView2Async();
+        MyWebView.NavigateToString("<html><body><p>Hello world!</p></body></html>");
     }
 }
