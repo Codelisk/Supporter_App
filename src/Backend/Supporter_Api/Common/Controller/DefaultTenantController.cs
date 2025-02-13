d25ec221d72ba84b54286fdc0183cc4305929168
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-02-13 10:50:29 +0100

    fix backend

--- src/Backend/Supporter_Api/Common/Controller/DefaultTenantController.cs
+++ src/Backend/Supporter_Api/Common/Controller/DefaultTenantController.cs
@@ -1,7 +1,9 @@
-﻿using Supporter_Api.Common.Manager;
+﻿using Codelisk.GeneratorAttributes.WebAttributes.Controller;
+using Supporter_Api.Common.Manager;
 
 namespace Supporter_Api.Common.Controller
 {
+    [DefaultController(TenantConstants.Tenant)]
     public class DefaultTenantController<TDto, TEntity> : BaseController<TDto, Guid, TEntity>
         where TDto : TenantBaseDto<Guid>
         where TEntity : class, ITenantBaseDto<Guid>
--- src/Backend/Supporter_Api/Common/Controller/DefaultUserController.cs
+++ src/Backend/Supporter_Api/Common/Controller/DefaultUserController.cs
@@ -1,12 +1,14 @@
-﻿using Supporter_Api.Common.Manager;
+﻿using Codelisk.GeneratorAttributes.WebAttributes.Controller;
+using Supporter_Api.Common.Manager;
 
 namespace Supporter_Api.Common.Controller
 {
+    [DefaultController(TenantConstants.User)]
     public class DefaultUserController<TDto, TEntity> : BaseController<TDto, Guid, TEntity>
         where TDto : UserBaseDto<Guid>
         where TEntity : class, IUserBaseDto<Guid>
     {
-        public DefaultUserController(DefaultUserManager<TDto, TEntity> manager)
+        public DefaultUserController(IDefaultUserManager<TDto, TEntity> manager)
             : base(manager) { }
     }
 }
