15ec1fd7feaa4f7cf967f0bdcbf51fa04d4b6453
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-02-21 15:47:44 +0100

    remove apiclient

--- src/Frontend/Supporter_Uno/Supporter_Uno/ApiClient/refitter.ps1
+++ src/Frontend/Supporter_Uno/Supporter_Uno/ApiClient/refitter.ps1
@@ -1,3 +1,3 @@
-refitter https://localhost:7209/openapi/v1.json --interface-only --namespace "Supporter_Dtos" --multiple-interfaces ByTag
+refitter https://orderlyzesupporter-a4gyd0chgjh9aah6.canadacentral-01.azurewebsites.net/openapi/v1.json --interface-only --namespace "Supporter_Dtos" --multiple-interfaces ByTag --operation-name-template '{operationName}'
 pause
 exit
