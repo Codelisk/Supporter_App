e357c1966a1da22b722765a58b47cdb2273f7240
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-02-21 21:54:07 +0100

    first chat version

--- src/Frontend/Supporter_Uno/Supporter_Uno/Presentation/Chats/ChatPageViewModel.cs
+++ src/Frontend/Supporter_Uno/Supporter_Uno/Presentation/Chats/ChatPageViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Supporter_AI.Services.OpenAI.AzureAI;
 using Supporter_Dtos;
 using Supporter_Uno.Common;
 using Supporter_Uno.Providers;
@@ -14,27 +16,67 @@ public partial class ChatPageViewModel : BasePageViewModel
     private readonly IAzureTopicMappingApi azureTopicMappingApi;
     private readonly IChatQuestionApi chatQuestionApi;
     private readonly IChatAnswerApi chatAnswerApi;
+    private readonly IAzureOpenAIChatService azureOpenAIChatService;
 
     public ChatPageViewModel(
         BaseVmServices baseVmServices,
         IAzureTopicMappingApi azureTopicMappingApi,
         IChatQuestionApi chatQuestionApi,
-        IChatAnswerApi chatAnswerApi
+        IChatAnswerApi chatAnswerApi,
+        IAzureOpenAIChatService azureOpenAIChatService
     )
         : base(baseVmServices)
     {
         this.azureTopicMappingApi = azureTopicMappingApi;
         this.chatQuestionApi = chatQuestionApi;
         this.chatAnswerApi = chatAnswerApi;
+        this.azureOpenAIChatService = azureOpenAIChatService;
     }
+
+    private AzureTopicMappingDto AzureTopicMappingDto;
 
     public override async void Initialize(NavigationEventArgs e)
     {
         base.Initialize(e);
 
-        var topic = (e.Parameter as AITopicDto);
-        var azureTopics = await azureTopicMappingApi.GetByTopicId(topic!.GetId());
-        if (azureTopics.Count == 0) { }
-        await chatQuestionApi.GetAll();
+        AITopicDto topic = (e.Parameter as AITopicDto)!;
+        if (topic is null)
+        {
+            throw new ArgumentNullException(nameof(topic));
+        }
+
+        var azureTopics = await azureTopicMappingApi.GetByTopicId(topic.GetId());
+        if (azureTopics.Count == 0)
+        {
+            var newAssistant = await azureOpenAIChatService.CreateAssistant(topic.Name, 0);
+            var newThread = await azureOpenAIChatService.CreateThreadAsync(topic.Name);
+            AzureTopicMappingDto = await azureTopicMappingApi.Add(
+                new AzureTopicMappingDto
+                {
+                    AssistantId = newAssistant.Value.Id,
+                    ThreadId = newThread.Value.Id,
+                    TopicId = topic.GetId(),
+                }
+            );
+        }
+        else
+        {
+            azureTopics.Last();
+        }
+        //await chatQuestionApi.GetAll();
+    }
+
+    public string Question { get; set; }
+
+    public ICommand ChatCommand => new AsyncRelayCommand(OnChatAsync);
+
+    private async Task OnChatAsync()
+    {
+        await azureOpenAIChatService.Chat(
+            Question,
+            threadId: AzureTopicMappingDto.ThreadId,
+            assistantId: AzureTopicMappingDto.AssistantId,
+            temperature: null
+        );
     }
 }
