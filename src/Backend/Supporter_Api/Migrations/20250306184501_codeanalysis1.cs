794a739b07d7b677797283f29be765756bbbd72b
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-03-06 19:45:19 +0100

    update migrations

---
+++ src/Backend/Supporter_Api/Migrations/20250306184501_codeanalysis1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Supporter_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class codeanalysis1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "trainingMessageEntity");
+
+            migrationBuilder.CreateTable(
+                name: "aIRepoEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_aIRepoEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "azureRepoMappingEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ThreadId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssistantId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RepoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_azureRepoMappingEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "chatTrainingMessageEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TopicId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_chatTrainingMessageEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "codeAnswerEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Owner = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_codeAnswerEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "codeQuestionEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RepoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_codeQuestionEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "codeTrainingMessageEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RepoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_codeTrainingMessageEntity", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "aIRepoEntity");
+
+            migrationBuilder.DropTable(
+                name: "azureRepoMappingEntity");
+
+            migrationBuilder.DropTable(
+                name: "chatTrainingMessageEntity");
+
+            migrationBuilder.DropTable(
+                name: "codeAnswerEntity");
+
+            migrationBuilder.DropTable(
+                name: "codeQuestionEntity");
+
+            migrationBuilder.DropTable(
+                name: "codeTrainingMessageEntity");
+
+            migrationBuilder.CreateTable(
+                name: "trainingMessageEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_trainingMessageEntity", x => x.Id);
+                });
+        }
+    }
+}
