6f5d8812c87b49c3d4dbaec778eb204a0b419b42
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-03-07 12:26:24 +0100

    migrations

---
+++ src/Backend/Supporter_Api/Migrations/20250307112604_codeanalysis2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Supporter_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class codeanalysis2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "aIRepoEntity",
+                newName: "Owner");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Owner",
+                table: "aIRepoEntity",
+                newName: "Url");
+        }
+    }
+}
