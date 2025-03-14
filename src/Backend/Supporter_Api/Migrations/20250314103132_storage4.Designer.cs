a81edf3fc1e35c31b46d0cad50a8fd402c1aad78
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-03-14 11:31:48 +0100

    migrations

--- src/Backend/Supporter_Api/Migrations/20250314103132_storage4.Designer.cs
+++ src/Backend/Supporter_Api/Migrations/20250314103132_storage4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Supporter_Api.Database;
 
@@ -11,9 +12,11 @@ using Supporter_Api.Database;
 namespace Supporter_Api.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250314103132_storage4")]
+    partial class storage4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
